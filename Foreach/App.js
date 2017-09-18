b38bf3c9ac64a4ef5de8b7f5c6d24d53f58191b8
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2017-09-18 16:18:57 -0300

    Foreach

--- Foreach/App.js
+++ Foreach/App.js
@@ -1,2 +1,12 @@
-var arr = ["Gabriel", "Rafael"];
+//foreach sem indice
+// let arr: Array<string> = ["Gabriel", "Rafael"];
+// for (var nome of arr ) {
+//     console.log(nome);
+// }
+//foreach com indice e valor
+var arr2 = ["Gabriel", "Rafael"];
+arr2.forEach(function (nome, value) {
+    console.log(nome);
+    console.log(value);
+});
 //# sourceMappingURL=App.js.map
--- Foreach/App.ts
+++ Foreach/App.ts
@@ -1,2 +1,17 @@
-let arr: Array<number> = ["Gabriel", "Rafael"];
+//foreach sem indice
 
+
+// let arr: Array<string> = ["Gabriel", "Rafael"];
+
+// for (var nome of arr ) {
+//     console.log(nome);
+// }
+
+//foreach com indice e valor
+
+let arr2: Array<string> = ["Gabriel", "Rafael"];
+
+arr2.forEach((nome, value) => {
+    console.log(nome);
+    console.log(value);
+});
