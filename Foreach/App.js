d00b6a8f7fa8e79d8105d15ff3bd07ce7260886a
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2017-09-18 18:46:22 -0300

    correções no Enumeração

--- Foreach/App.js
+++ Foreach/App.js
@@ -1,13 +1,13 @@
 //foreach sem indice
 var arr = ["Gabriel", "Rafael"];
-for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
-    var nome = arr_1[_i];
+for (var nome in arr) {
     console.log(nome);
 }
+//OBS: colocando o "of" ele irá imprimir os valores de cada índice, colocando "in" ele irá imprimir os índices.
 //foreach com indice e valor
 // let arr2: Array<string> = ["Gabriel", "Rafael"];
 // arr2.forEach((nome, value) => {
 //     console.log(nome);
 //     console.log(value);
-// }); 
+// });
 //# sourceMappingURL=App.js.map
