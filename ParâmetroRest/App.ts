ee8fbfec843c5b0a00b8e40a55fe5a9f245c0896
Gabriel Silva <gabriel-silva_2011@hotmail.com>
2017-09-19 15:19:52 -0300

    adicionando texto no App => Parâmentros Rest

--- ParâmetroRest/App.ts
+++ ParâmetroRest/App.ts
@@ -1,5 +1,13 @@
-function print(nome: string, ...sobreNome:string[]){
-    return nome + " " + sobreNome.join(" ");
+/*Parâmentros Rest
+Quando você não sabe quantos parâmetros deseja passar para uma determinada
+função ao mesmo tempo. Os parâmentros Rest são procedidos por: ... e seguidos por colchetes []
+
+Ex:
+
+*/
+
+function print(nome: string, ...sobrenome:string[]){
+    return nome + " " + sobrenome.join(" ");
 }
 
 console.log(print("José", "Gabriel", "Neves", "da", "Silva"));
